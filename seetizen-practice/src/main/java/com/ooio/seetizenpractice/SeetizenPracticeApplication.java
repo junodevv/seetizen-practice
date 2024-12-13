da2d05337989991188e6fba66731e160b789bc25
leejuno <zhqkfxm0127@naver.com>
2024-12-13 15:06:48 +0900

    chore: init

---
+++ seetizen-practice/src/main/java/com/ooio/seetizenpractice/SeetizenPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ooio.seetizenpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeetizenPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeetizenPracticeApplication.class, args);
+    }
+
+}
---
+++ seetizen-practice/src/test/java/com/ooio/seetizenpractice/SeetizenPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ooio.seetizenpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeetizenPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
